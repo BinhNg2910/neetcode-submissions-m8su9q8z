84388118808038eb6d4551fcc0c84236d9458b71
Brandon <93148667+BinhNg2910@users.noreply.github.com>
2026-04-15 00:42:44 +1000

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        1. using a list with 26 element to store frequency of each char 
+        and use a key in hashtable
+        TC: O(n*k)
+        SC: O(n*k)
+        with n is number of string, k is max length of word in strs
+        """
+        # def convertToKey(s: string) -> tuple:
+        #     key = [0] * 26
+        #     for c in s:
+        #         key[ord(c) - ord("a")] += 1
+        #     return tuple(key)
+
+        # anaGr = defaultdict(list)
+        # for s in strs:
+        #     anaGr[convertToKey(s)].append(s)
+        # return list(anaGr.values())
+        
+        """
+        2. sort all characters with alphabet order then string as key
+        TC: O(n*logk)
+        SC: O(n)
+        """
+        def generateKey(s: str) -> str:
+            key = sorted(s)
+            # print(s, key)
+            return "".join(key)
+
+        anaGr = defaultdict(list)
+        for s in strs:
+            anaGr[generateKey(s)].append(s)
+        return list(anaGr.values())
+
