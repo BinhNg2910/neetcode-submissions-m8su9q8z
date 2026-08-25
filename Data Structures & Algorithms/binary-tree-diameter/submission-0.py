2144b5700581ef4b7ae1d32fa32cb01749a36b2e
Brandon <93148667+BinhNg2910@users.noreply.github.com>
2026-08-25 15:13:24 +1000

    Add: binary-tree-diameter - submission-0

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-0.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        """
+        Idea: make a recursion function to return max length of just one path from root to its leaf, get max on left and right then compare which the global max diamter
+        TC: O(2^n)
+        SC: O(h) -> O(n) -> h = n
+        n is number of node
+        """
+        def dfs(node: Optional[TreeNode]) -> int:
+            nonlocal maxDiameter
+            if node is None:
+                return 0
+            maxLeft = dfs(node.left)
+            maxRight = dfs(node.right)
+            maxDiameter = max(maxDiameter, maxLeft + maxRight)
+            return 1 + max(maxLeft, maxRight)
+
+        maxDiameter = 0
+        _ = dfs(root)
+        return maxDiameter
+        
